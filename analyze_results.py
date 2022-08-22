d8b514dfab6e952823abd690eb0ec44003f7d7d6
Jason Toney <toneyja@oregonstate.edu>
2022-08-22 13:44:08 -0700

    analyze_results.py: log the ineffective treatment with its image

--- analyze_results.py
+++ analyze_results.py
@@ -43,6 +43,11 @@ def analyze_results(plate, isolate):
     )
 
     for block in range(96):
+        if len(str(block)) == 1:
+            img_prefix = "Image_000"
+        else:
+            img_prefix = "Image_00"
+        img_name = img_prefix + str(block) + ".jpg"
         area_change = round(_48hr_results[block][0] - _0hr_results[block][0], 3)
         perim_change = round(_48hr_results[block][1] - _0hr_results[block][1], 3)
         angle_change = round(_48hr_results[block][2] - _0hr_results[block][2], 3)
@@ -54,10 +59,7 @@ def analyze_results(plate, isolate):
         round_change = round(_48hr_results[block][8] - _0hr_results[block][8], 3)
         solidity_change = round(_48hr_results[block][9] - _0hr_results[block][9], 3)
         if get_treatments(plate, block) == CNTL:
-            if len(str(block)) == 1:
-                cntl_imgs.append("Image_000" + str(block) + ".jpg")
-            else:
-                cntl_imgs.append("Image_00" + str(block) + ".jpg")
+            cntl_imgs.append(img_name)
             cntl_area.append(area_change)
             cntl_perim.append(perim_change)
             cntl_angle.append(angle_change)
@@ -72,10 +74,7 @@ def analyze_results(plate, isolate):
             if area_change > 23 and perim_change > 5 and round_change < -0.1:
                 if not resistant.count(get_treatments(plate, block)):
                     resistant.append(get_treatments(plate, block))
-                if len(str(block)) == 1:
-                    resistant_imgs.append("Image_000" + str(block) + ".jpg")
-                else:
-                    resistant_imgs.append("Image_00" + str(block) + ".jpg")
+                resistant_imgs.append(img_name + " : " + get_treatments(plate, block))
             else:
                 if not efficacious.count(get_treatments(plate, block)):
                     efficacious.append(get_treatments(plate, block))
