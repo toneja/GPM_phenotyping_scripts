f163bcb8cde367f4f77e43671a78776d15b9a67d
Jason Toney <toneyja@oregonstate.edu>
2023-06-06 16:18:54 -0700

    eval_model.py: read testing/training data from separate files
    - maintain static testing datasets instead of on-the-fly splits

--- models/eval_model.py
+++ models/eval_model.py
@@ -20,7 +20,7 @@
 """
 Evaluate a predictive model using logistic regression with feature selection.
 
-Usage: eval_model.py <csv_filename> [num_runs]
+Usage: eval_model.py <train_csv_filename> <test_csv_filename>
 
 Read a training dataset from a CSV file and perform logistic regression with feature selection.
 Evaluate the model's accuracy in predicting the output label and print the evaluation metrics.
@@ -32,21 +32,28 @@ import pandas as pd
 import warnings
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import precision_score, recall_score, f1_score, confusion_matrix
-from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import SequentialFeatureSelector
-from tqdm import tqdm
 
 
-def evaluate_predictive_model(csv_filename, num_runs=10):
+def evaluate_predictive_model(train_csv_filename, test_csv_filename):
     """Evaluate predictive model using logistic regression with feature selection."""
-    # Load dataset from CSV file
-    dataset = pd.read_csv(csv_filename)
+    # Load training dataset from CSV file
+    train_dataset = pd.read_csv(train_csv_filename)
 
-    # Prepare input features and output label
-    filename = os.path.splitext(os.path.basename(csv_filename))[0]
-    output_label = filename.split("_")[0]
-    _x = dataset.drop(output_label, axis=1)
-    _y = dataset[output_label]
+    # Load testing dataset from CSV file
+    test_dataset = pd.read_csv(test_csv_filename)
+
+    # Prepare input features and output label for training set
+    train_filename = os.path.splitext(os.path.basename(train_csv_filename))[0]
+    train_output_label = train_filename.split("_")[0]
+    train_x = train_dataset.drop(train_output_label, axis=1)
+    train_y = train_dataset[train_output_label]
+
+    # Prepare input features and output label for testing set
+    test_filename = os.path.splitext(os.path.basename(test_csv_filename))[0]
+    test_output_label = test_filename.split("_")[0]
+    test_x = test_dataset.drop(test_output_label, axis=1)
+    test_y = test_dataset[test_output_label]
 
     # Create an empty DataFrame to store the results
     results_df = pd.DataFrame(
@@ -60,80 +67,56 @@ def evaluate_predictive_model(csv_filename, num_runs=10):
         ]
     )
 
-    best_accuracy = 0.0
-    best_feature_set = ""
-
     # ignore some annoying warnings from the code below
     warnings.filterwarnings(
         "ignore", category=FutureWarning, module="sklearn.feature_selection"
     )
 
-    for i in tqdm(range(num_runs), unit="tests"):
-        # Split dataset into training and testing sets
-        _x_train, _x_test, _y_train, _y_test = train_test_split(
-            _x, _y, test_size=0.5, random_state=None
-        )
+    # Use stepwise feature selection to find the best set of features
+    logreg = LogisticRegression(solver="liblinear", multi_class="ovr")
+    sfs = SequentialFeatureSelector(logreg, n_features_to_select=None)
+    sfs.fit(train_x, train_y)
+    train_x_sfs = sfs.transform(train_x)
+    test_x_sfs = sfs.transform(test_x)
 
-        # Use stepwise feature selection to find the best set of features
-        logreg = LogisticRegression(solver="liblinear", multi_class="ovr")
-        sfs = SequentialFeatureSelector(logreg, n_features_to_select=None)
-        sfs.fit(_x_train, _y_train)
-        _x_train_sfs = sfs.transform(_x_train)
-        _x_test_sfs = sfs.transform(_x_test)
+    # Train a logistic regression model with selected features
+    logreg.fit(train_x_sfs, train_y)
 
-        # Train a logistic regression model with selected features
-        logreg.fit(_x_train_sfs, _y_train)
+    # Evaluate model's accuracy on the testing set
+    accuracy = logreg.score(test_x_sfs, test_y)
 
-        # Evaluate model's accuracy on the testing set
-        accuracy = logreg.score(_x_test_sfs, _y_test)
+    # Predict labels for the testing set
+    y_pred = logreg.predict(test_x_sfs)
 
-        # Predict labels for the testing set
-        y_pred = logreg.predict(_x_test_sfs)
+    # Calculate precision, recall, and F1-score
+    precision = precision_score(test_y, y_pred)
+    recall = recall_score(test_y, y_pred)
+    f1 = f1_score(test_y, y_pred)
 
-        # Calculate precision, recall, and F1-score
-        precision = precision_score(_y_test, y_pred)
-        recall = recall_score(_y_test, y_pred)
-        f1 = f1_score(_y_test, y_pred)
+    # Calculate confusion matrix
+    tn, fp, fn, tp = confusion_matrix(test_y, y_pred).ravel()
 
-        # Calculate confusion matrix
-        tn, fp, fn, tp = confusion_matrix(_y_test, y_pred).ravel()
+    # Calculate specificity
+    specificity = tn / (tn + fp)
 
-        # Calculate specificity
-        specificity = tn / (tn + fp)
-
-        # Store the results in the DataFrame
-        selected_features = ", ".join(_x.columns[sfs.get_support()])
-        results_df.loc[i] = [
-            selected_features,
-            accuracy,
-            precision,
-            recall,
-            specificity,
-            f1,
-        ]
-
-        # Update the best feature set if the accuracy is higher
-        if accuracy > best_accuracy:
-            best_accuracy = accuracy
-            best_feature_set = selected_features
+    # Store the results in the DataFrame
+    selected_features = ", ".join(train_x.columns[sfs.get_support()])
+    results_df.loc[0] = [
+        selected_features,
+        accuracy,
+        precision,
+        recall,
+        specificity,
+        f1,
+    ]
 
     # Print the results
-    print(f"Results [{csv_filename}]:")
-    print(results_df.to_string())
-    print("\nAverage:")
-    print(results_df.iloc[:, 1:].mean().to_string())
-    print("\nBest Feature Set:")
-    print(f"{best_feature_set} @ {best_accuracy}")
-    print(
-        "--------------------------------------------------------------------------------------"
-    )
+    print(f"Results [{train_csv_filename} - {test_csv_filename}]:")
+    print(results_df.to_string(index=False))
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        if len(sys.argv) > 2:
-            evaluate_predictive_model(sys.argv[1], int(sys.argv[2]))
-        else:
-            evaluate_predictive_model(sys.argv[1])
+    if len(sys.argv) > 2:
+        evaluate_predictive_model(sys.argv[1], sys.argv[2])
     else:
-        print("Please provide the CSV filename as an argument.")
+        print("Please provide the training and testing CSV filenames as arguments.")
