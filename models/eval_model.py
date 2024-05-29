eb6b46460033f171f2c028cae1ea304c8d8f310b
unchurchable1 <toneyja@oregonstate.edu>
2024-05-29 13:28:18 -0700

    eval_model: fix up ROC plot

--- models/eval_model.py
+++ models/eval_model.py
@@ -135,13 +135,13 @@ def evaluate_predictive_model(train_csv_filename, test_csv_filename):
 
     # Plot the ROC curve
     plt.figure()
-    plt.plot(fpr, tpr, color="blue", lw=2, label="ROC curve (area = %0.2f)" % auc)
+    plt.plot(fpr, tpr, color="blue", lw=2, label=f"ROC curve (area = {round(auc, 5)})")
     plt.plot([0, 1], [0, 1], color="grey", lw=2, linestyle="--")
-    plt.xlim([0.0, 1.0])
+    plt.xlim([0.0, 1.05])
     plt.ylim([0.0, 1.05])
-    plt.xlabel("False Positive Rate")
-    plt.ylabel("True Positive Rate")
-    plt.title("Receiver Operating Characteristic")
+    plt.xlabel("False Positive Rate (1 - Specificity)")
+    plt.ylabel("True Positive Rate (Sensitivity)")
+    plt.title(f"{train_output_label.capitalize()} Model")
     plt.legend(loc="lower right")
     plt.show()
 
