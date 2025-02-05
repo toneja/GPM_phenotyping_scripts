d44791d7609b14f733a5d6edc9503aad48c22d15
toneja <toneyja@oregonstate.edu>
2025-02-04 16:19:05 -0800

    update.py: add timeout, header, docstrings

--- setup/update.py
+++ setup/update.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
+#
+# This file is part of the GPM phenotyping scripts.
+#
+# Copyright (c) 2025 Jason Toney
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+"""This script updates this repository with the latest version from Github."""
 
 import io
 import os
@@ -8,10 +27,11 @@ import zipfile
 
 
 def main():
+    """Download and install the update zip file."""
     # Download the zip file
     URL = "https://github.com/toneja/GPM_phenotyping_scripts/archive/refs/heads/revolution.zip"
     print(f"Downloading update package from {URL}")
-    archive = requests.get(URL)
+    archive = requests.get(URL, timeout=5)
     # Unpack the zip file
     print("Extracting update files...")
     with zipfile.ZipFile(io.BytesIO(archive.content)) as zip_arc:
