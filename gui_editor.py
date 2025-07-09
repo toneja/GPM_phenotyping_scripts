110ed9b9e4b0a774d458205f7d20c4f8d3044929
Jason Toney <toneyja@oregonstate.edu>
2025-07-09 15:08:21 -0700

    GUI: row indices: return int if index is singular

--- gui_editor.py
+++ gui_editor.py
@@ -290,7 +290,7 @@ class ExcelDataEditor:
         tags = []
         for item in list(selections):
             tags.append(int(self.tree.item(item, "tags")[0]))
-        return tags if tags else None
+        return (tags if len(tags) > 1 else tags[0]) if tags else None
 
     def add_row(self):
         if not self.current_sheet:
@@ -417,7 +417,7 @@ class ExcelDataEditor:
 
         column = self.tree.identify_column(event.x)
         col_index = int(column.replace("#", "")) - 1  # No index column offset needed
-        row_index = self.get_selected_row_indices()[0]
+        row_index = self.get_selected_row_indices()
 
         if row_index is None:
             return
